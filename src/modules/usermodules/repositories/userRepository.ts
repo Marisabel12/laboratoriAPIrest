69ad29f67c0aa0b3632e1e6ff7d8342df2f0d1a8
Marisabel12 <91963027+Marisabel12@users.noreply.github.com>
2021-10-14 12:13:59 -0400

    Add files via upload

---
+++ src/modules/usermodules/repositories/userRepository.ts
@@ -0,0 +1,7 @@
+import { IUser } from "../model/UserModel";
+import { BaseRepository } from "./base/BaseRepository";
+
+class UserRepository extends BaseRepository<IUser> {
+    public login(){}
+    public singOut(){}
+} 
